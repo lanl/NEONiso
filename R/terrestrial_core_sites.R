495a274aa5c368fab2389079cabbb236c8b34182
Rich Fiorella <rich.fiorella@utah.edu>
2019-05-23 21:47:11 -0600

    Add two basic functions that return a vector of 4-letter site codes for core or relocatable terrestrial sites.

---
+++ R/terrestrial_core_sites.R
@@ -0,0 +1,15 @@
+#' terrestrial_core_sites
+#'
+#' @author Rich Fiorella \email{rich.fiorella@@utah.edu}
+
+terrestrial_core_sites <- function() {
+
+  # core sites as of 190523 based on neon update from 181029.
+
+  core.sites <- c("BONA","CLBJ","CPER","GUAN","HARV","KONZ",
+                  "NIWO","ONAQ","ORNL","OSBS","PUUM","SCBI",
+                  "SJER","SRER","TALL","TOOL","UNDE","WOOD",
+                  "WREF","YELL")
+
+  return(core.sites)
+}
---
+++ R/terrestrial_relocatable_sites.R
@@ -0,0 +1,17 @@
+#' terrestrial_relocatable_sites
+#'
+#' @author Rich Fiorella \email{rich.fiorella@@utah.edu}
+
+terrestrial_relocatable_sites <- function() {
+
+  # relocatable sites as of 190523 based on neon update from 181029.
+
+  reloc.sites <- c("ABBY","BARR","BART","BLAN","DCFS","DEJU",
+                   "DELA","DSNY","GRSM","HEAL","JERC","JORN",
+                   "KONA","LAJA","LENO","MLBS","MOAB","NOGP",
+                   "OAES","RMNP","SERC","SOAP","STEI","STER",
+                   "TEAK","TREE","UKFS")
+
+  return(reloc.sites)
+
+}
