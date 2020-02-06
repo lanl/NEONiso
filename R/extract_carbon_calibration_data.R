9a0876d48abed6a9a05a3e9e9c1a4c00c0155ecb
Rich Fiorella <rich.fiorella@utah.edu>
2020-02-06 09:39:33 -0700

    Add switch for choosing uncertainty source. Set default to pulling it from the data group. Closes #1

--- R/extract_carbon_calibration_data.R
+++ R/extract_carbon_calibration_data.R
@@ -3,12 +3,13 @@
 #' @param standard String indicating whether to grab data from the high, medium, or low standard.
 #' @param data.list List containing data, from the /*/dp01/data/ group in NEON HDF5 file.
 #' @param ucrt.list List containing uncertainty data, from the /*/dp01/ucrt/ group in NEON HDF5 file.
+#' @param ucrt.source Where in the HDF5 file should the variance be extracted from? (Valid values are "data" and "ucrt")
 #'
 #' @return Returns data frame of required variables from /data/ and /ucrt/ groups.
 #' @export
 #'
 #' @examples
-extract_carbon_calibration_data <- function(data.list,ucrt.list,standard) {
+extract_carbon_calibration_data <- function(data.list,ucrt.list,standard,ucrt.source="data") {
   
   require(dplyr)
   
@@ -25,20 +26,42 @@ extract_carbon_calibration_data <- function(data.list,ucrt.list,standard) {
     stop()
   }
   
-  std.df <- data.frame(d13C_obs_mean=data$dlta13CCo2$mean,
-            d13C_obs_n=data$dlta13CCo2$numSamp,
-            d13C_obs_btime=data$dlta13CCo2$timeBgn,
-            d13C_obs_etime=data$dlta13CCo2$timeEnd,
-            d13C_ref_mean=data$dlta13CCo2Refe$mean,
-            d13C_ref_btime=data$dlta13CCo2Refe$timeBgn,
-            d13C_ref_etime=data$dlta13CCo2Refe$timeEnd,
-            d13C_obs_var=ucrt$dlta13CCo2$vari,
-            d13C_ref_var=data$dlta13CCo2Refe$vari,
-            CO2_obs_mean=data$rtioMoleDryCo2$mean,
-            CO2_obs_n=data$rtioMoleDryCo2$numSamp,
-            CO2_ref_mean=data$rtioMoleDryCo2Refe$mean,
-            CO2_ref_var=data$rtioMoleDryCo2Refe$vari,
-            CO2_obs_var=ucrt$rtioMoleDryCo2$vari)
+  if (ucrt.source == "ucrt") {
+    
+    std.df <- data.frame(d13C_obs_mean=data$dlta13CCo2$mean,
+                         d13C_obs_n=data$dlta13CCo2$numSamp,
+                         d13C_obs_btime=data$dlta13CCo2$timeBgn,
+                         d13C_obs_etime=data$dlta13CCo2$timeEnd,
+                         d13C_ref_mean=data$dlta13CCo2Refe$mean,
+                         d13C_ref_btime=data$dlta13CCo2Refe$timeBgn,
+                         d13C_ref_etime=data$dlta13CCo2Refe$timeEnd,
+                         d13C_obs_var=ucrt$dlta13CCo2$vari,
+                         d13C_ref_var=data$dlta13CCo2Refe$vari,
+                         CO2_obs_mean=data$rtioMoleDryCo2$mean,
+                         CO2_obs_n=data$rtioMoleDryCo2$numSamp,
+                         CO2_ref_mean=data$rtioMoleDryCo2Refe$mean,
+                         CO2_ref_var=data$rtioMoleDryCo2Refe$vari,
+                         CO2_obs_var=ucrt$rtioMoleDryCo2$vari)
+    
+  } else if (ucrt.source == "data") {
+    
+    std.df <- data.frame(d13C_obs_mean=data$dlta13CCo2$mean,
+                         d13C_obs_n=data$dlta13CCo2$numSamp,
+                         d13C_obs_btime=data$dlta13CCo2$timeBgn,
+                         d13C_obs_etime=data$dlta13CCo2$timeEnd,
+                         d13C_ref_mean=data$dlta13CCo2Refe$mean,
+                         d13C_ref_btime=data$dlta13CCo2Refe$timeBgn,
+                         d13C_ref_etime=data$dlta13CCo2Refe$timeEnd,
+                         d13C_obs_var=data$dlta13CCo2$vari,
+                         d13C_ref_var=data$dlta13CCo2Refe$vari,
+                         CO2_obs_mean=data$rtioMoleDryCo2$mean,
+                         CO2_obs_n=data$rtioMoleDryCo2$numSamp,
+                         CO2_ref_mean=data$rtioMoleDryCo2Refe$mean,
+                         CO2_ref_var=data$rtioMoleDryCo2Refe$vari,
+                         CO2_obs_var=data$rtioMoleDryCo2$vari)
+    
+  }
+
   
   # add standard name
   std.df <- std.df %>%
