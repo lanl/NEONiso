2acecac928e650e78922aa3c0197776f25b98b35
Rich Fiorella <rich.fiorella@utah.edu>
2020-03-02 17:15:14 -0700

    Fix sync issue with calibrate_carbon_linreg

--- R/calibrate_carbon_linreg.R
+++ R/calibrate_carbon_linreg.R
@@ -12,8 +12,6 @@
 #' @return nothing to the workspace, but creates a new output file of calibrated carbon isotope data.
 #' @export
 
-#' 
-
 calibrate_carbon_linreg <- function(inname,outname,site,time.diff.betweeen.standards=1800,
                                             force.cal.to.beginning=TRUE,force.cal.to.end=TRUE){
   
@@ -28,6 +26,28 @@ calibrate_carbon_linreg <- function(inname,outname,site,time.diff.betweeen.stand
   med_rs  <- extract_carbon_calibration_data(ciso,ucrt,"med")
   low_rs  <- extract_carbon_calibration_data(ciso,ucrt,"low")
   
+  # cut out period where there appears to be a valve malfunction.
+  high_rs <- high_rs %>%
+    mutate(dom = lubridate::day(d13C_obs_btime)) %>%
+    group_by(dom) %>%
+    filter(d13C_obs_n > 200 | is.na(d13C_obs_n)) %>%
+    slice(1) %>%
+    ungroup()
+  
+  med_rs <- med_rs %>%
+    mutate(dom = lubridate::day(d13C_obs_btime)) %>%
+    group_by(dom) %>%
+    filter(d13C_obs_n > 200 | is.na(d13C_obs_n)) %>%
+    slice(1) %>%
+    ungroup()  
+  
+  low_rs <- low_rs %>%
+    mutate(dom = lubridate::day(d13C_obs_btime)) %>%
+    group_by(dom) %>%
+    filter(d13C_obs_n > 200 | is.na(d13C_obs_n)) %>%
+    slice(1) %>%
+    ungroup()
+
   #=======================================================================
   # apply calibration routines
   #======================================================================= 
@@ -35,76 +55,116 @@ calibrate_carbon_linreg <- function(inname,outname,site,time.diff.betweeen.stand
   stds <- do.call(rbind,list(high_rs,med_rs,low_rs))
   rm(high_rs,med_rs,low_rs)
   
-  # replace NaNs with NA
-  # rpf note on 181121 - what does this line actually do? Seems tautological.
-  # rpf note 181126 - is.na() also returns NaN as NA, so this does actually do what first
-  # comment indicates.
-  stds[ is.na(stds) ] <- NA
-  
-  # change class of time variables from charatcter to posixct.
-  stds$d13C_obs_btime <- as.POSIXct(stds$d13C_obs_btime,format="%Y-%m-%dT%H:%M:%S.%OSZ",tz="UTC")
-  stds$d13C_obs_etime <- as.POSIXct(stds$d13C_obs_etime,format="%Y-%m-%dT%H:%M:%S.%OSZ",tz="UTC")
-  
-  stds$d13C_ref_btime <- as.POSIXct(stds$d13C_ref_btime,format="%Y-%m-%dT%H:%M:%S.%OSZ",tz="UTC")
-  stds$d13C_ref_etime <- as.POSIXct(stds$d13C_ref_etime,format="%Y-%m-%dT%H:%M:%S.%OSZ",tz="UTC")
-  
-  # reorder data frame
-  stds <- stds[order(stds$d13C_obs_btime),]
-  
-  # assign a vector corresponding to calibration period.
-  stds$cal_period <- stds$d13C_obs_n
-  
-  period_id <- 1
-  tdiffs <- c(diff(stds$d13C_obs_btime),0)
-  for (i in 1:nrow(stds)) {
-    stds$cal_period[i] <- period_id   
-    if (tdiffs[i] >= time.diff.betweeen.standards) {period_id = period_id + 1}
-  }
-  
-  # okay, now run calibrations...
-  #------------------------------
-  # create output variables.
-  cal_slopes <- vector()
-  cal_ints   <- vector()
-  cal_rsq    <- vector()
-  
-  for (i in 2:max(stds$cal_period)) {
-    # subset data.
-    cal.subset <- stds[which(stds$cal_period==i | stds$cal_period==(i-1)),]
+  if (nrow(stds) > 0) {
+    # replace NaNs with NA
+    # rpf note on 181121 - what does this line actually do? Seems tautological.
+    # rpf note 181126 - is.na() also returns NaN as NA, so this does actually do what first
+    # comment indicates.
+    stds[ is.na(stds) ] <- NA
     
-    if (!all(is.na(cal.subset$d13C_obs_mean)) & !all(is.na(cal.subset$d13C_ref_mean))) { # ensure that not all cases are missing.
-      tmp <- lm(d13C_ref_mean ~ d13C_obs_mean,data=cal.subset)
-      
-      cal_slopes[i-1] <- coef(tmp)[[2]]
-      cal_ints[i-1] <- coef(tmp)[[1]]
-      cal_rsq[i-1] <- summary(tmp)$r.squared
-    } else {
-      cal_slopes[i-1] <- NA
-      cal_ints[i-1] <- NA
-      cal_rsq[i-1] <- NA
+    # change class of time variables from charatcter to posixct.
+    stds$d13C_obs_btime <- as.POSIXct(stds$d13C_obs_btime,format="%Y-%m-%dT%H:%M:%S.%OSZ",tz="UTC")
+    stds$d13C_obs_etime <- as.POSIXct(stds$d13C_obs_etime,format="%Y-%m-%dT%H:%M:%S.%OSZ",tz="UTC")
+    
+    stds$d13C_ref_btime <- as.POSIXct(stds$d13C_ref_btime,format="%Y-%m-%dT%H:%M:%S.%OSZ",tz="UTC")
+    stds$d13C_ref_etime <- as.POSIXct(stds$d13C_ref_etime,format="%Y-%m-%dT%H:%M:%S.%OSZ",tz="UTC")
+    
+    # reorder data frame
+    stds <- stds[order(stds$d13C_obs_btime),]
+    
+    # assign a vector corresponding to calibration period.
+    stds$cal_period <- stds$d13C_obs_n
+    
+    period_id <- 1
+    tdiffs <- c(diff(stds$d13C_obs_btime),0)
+    for (i in 1:nrow(stds)) {
+      stds$cal_period[i] <- period_id   
+      if (tdiffs[i] >= time.diff.betweeen.standards) {period_id = period_id + 1}
     }
+    
+    
+    # okay, now run calibrations...
+    #------------------------------
+    # create output variables.
+    cal_slopes <- vector()
+    cal_ints   <- vector()
+    cal_rsq    <- vector()
+    calRmse <- vector()
+    
+    
+    for (i in 2:max(stds$cal_period)) {
+      # subset data.
+      cal.subset <- stds[which(stds$cal_period==i | stds$cal_period==(i-1)),]
+      
+      #---------------------------------------------
+      # do some light validation of these points.
+      cal.subset <- cal.subset %>%
+        filter(d13C_obs_var < 5 & abs(CO2_obs_mean - CO2_ref_mean) < 15)
+      
+      if (length(unique(cal.subset$std_name)) >= 2 & # ensure at least 2 standards are present
+          !all(is.na(cal.subset$d13C_obs_mean)) & # ensure that not all observational values are missing
+          !all(is.na(cal.subset$d13C_ref_mean))) { # ensure that not all reference values are missing.
+        
+        tmpmod <- lm(d13C_ref_mean ~ d13C_obs_mean,data=cal.subset)
+        
+        cal_slopes[i-1] <- coef(tmpmod)[[2]]
+        cal_ints[i-1] <- coef(tmpmod)[[1]]
+        cal_rsq[i-1] <- summary(tmpmod)$r.squared
+        
+        if (any(cal.subset$std_name == "med")) {
+          # get medium vars.
+          tmp <- subset(cal.subset, std_name == "med")
+          
+          calRmse[i-1] <- mean(sqrt(((tmp$d13C_obs_mean*cal_slopes[i-1] + cal_ints[i-1]) - tmp$d13C_ref_mean)^2),
+                                  na.rm = TRUE)
+          
+        } else {
+          
+          calRmse[i-1] <- NA
+          
+        }
+        
+      } else {
+        
+        cal_slopes[i-1] <- NA
+        cal_ints[i-1] <- NA
+        cal_rsq[i-1] <- NA
+        calRmse[i-1] <- NA
+        
+      }
+    }
+    
+    # make dataframe of calibration data.
+    times <- stds %>%
+      select(d13C_obs_btime,d13C_obs_etime,d13C_ref_btime,d13C_ref_etime,cal_period) %>%
+      group_by(cal_period) %>%
+      summarize(etime = max(c(d13C_obs_etime,d13C_ref_etime)))
+    
+    # loop through times, assign beginning, ending value. max etime should be just fine.
+    starttimes <- vector()
+    endtimes <- vector()
+    
+    for (i in 1:length(cal_slopes)) {
+      starttimes[i] <- times$etime[i]
+      endtimes[i] <- times$etime[i+1]
+    }
+    
+    # output dataframe giving valid time range, slopes, intercepts, rsquared.
+    out <- data.frame(start=as.POSIXct(starttimes,tz="UTC",origin="1970-01-01"),
+                      end=as.POSIXct(endtimes,tz="UTC",origin="1970-01-01"),
+                      slope=cal_slopes,intercept=cal_ints,r2=cal_rsq,
+                      calRmse)
+    
+  } else {
+
+        # output dataframe giving valid time range, slopes, intercepts, rsquared.
+    out <- data.frame(start=as.POSIXct(as.Date("1970-01-01"),tz="UTC",origin="1970-01-01"),
+                      end=as.POSIXct(as.Date("1970-01-01"),tz="UTC",origin="1970-01-01"),
+                      slope=as.numeric(NA),intercept=as.numeric(NA),r2=as.numeric(NA),
+                      calRmse=as.numeric(NA))
   }
   
-  # make dataframe of calibration data.
-  times <- stds %>%
-    select(d13C_obs_btime,d13C_obs_etime,d13C_ref_btime,d13C_ref_etime,cal_period) %>%
-    group_by(cal_period) %>%
-    summarize(etime = max(c(d13C_obs_etime,d13C_ref_etime)))
-  
-  # loop through times, assign beginning, ending value. max etime should be just fine.
-  starttimes <- vector()
-  endtimes <- vector()
-  
-  for (i in 1:length(cal_slopes)) {
-    starttimes[i] <- times$etime[i]
-    endtimes[i] <- times$etime[i+1]
-  }
-  
-  # output dataframe giving valid time range, slopes, intercepts, rsquared.
-  out <- data.frame(start=as.POSIXct(starttimes,tz="UTC",origin="1970-01-01"),
-                    end=as.POSIXct(endtimes,tz="UTC",origin="1970-01-01"),
-                    slope=cal_slopes,intercept=cal_ints,r2=cal_rsq)
-  
+ 
   var_for_h5 <- out
   
   var_for_h5$start <- convert_POSIXct_to_NEONhdf5_time(out$start)
@@ -136,7 +196,8 @@ calibrate_carbon_linreg <- function(inname,outname,site,time.diff.betweeen.stand
   
   H5Gclose(attrloc)
   
-  co2.cal.outloc <- H5Gopen(fid,paste0('/',site,'/dp01/data/isoCo2'))
+  h5createGroup(outname,paste0('/',site,'/dp01/data/isoCo2/calData'))
+  co2.cal.outloc <- H5Gopen(fid,paste0('/',site,'/dp01/data/isoCo2/calData'))
   
   # write out dataset.
   h5writeDataset.data.frame(obj = var_for_h5,h5loc=co2.cal.outloc,name="calRegressions",DataFrameAsCompound = TRUE)
