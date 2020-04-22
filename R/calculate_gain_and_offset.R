371a38a78db05b82ebbdce69d0474a4efc5d3b91
Rich Fiorella <rich.fiorella@utah.edu>
2020-04-22 11:24:38 -0600

    Edit calculate_gain_and_offset on bowling-qaqc-calculations branch to match master.

--- R/calculate_gain_and_offset.R
+++ R/calculate_gain_and_offset.R
@@ -19,24 +19,24 @@ calculate_gain_and_offset <- function(std1,std2) {
   offset12C <- std1$conc12CCO2_ref - gain12C*std1$conc12CCO2_obs
   offset13C <- std1$conc13CCO2_ref - gain13C*std1$conc13CCO2_obs
   
-  # calculate uncertainty on gain values.
-  vari.g12C <- (std1$vari12CCO2_ref + std2$vari12CCO2_ref)/ # already variance, so already sigma^2
-    (std1$conc12CCO2_obs - std2$conc12CCO2_obs)^2 +
-    (std1$vari12CCO2_obs + std2$vari12CCO2_obs)* # already variance, so already sigma^2
-    ((std1$conc12CCO2_ref - std2$conc12CCO2_ref)/(std1$conc12CCO2_obs - std2$conc12CCO2_obs)^2)^2
-  
-  vari.g13C <- (std1$vari13CCO2_ref + std2$vari13CCO2_ref)/ # already variance, so already sigma^2
-    (std1$conc13CCO2_obs - std2$conc13CCO2_obs)^2 +
-    (std1$vari13CCO2_obs + std2$vari13CCO2_obs)* # already variance, so already sigma^2
-    ((std1$conc13CCO2_ref - std2$conc13CCO2_ref)/(std1$conc13CCO2_obs - std2$conc13CCO2_obs)^2)^2
-  
-  vari.o12C <- std1$vari12CCO2_ref + std1$conc12CCO2_obs^2*vari.g12C + 
-    gain12C^2*std1$vari12CCO2_obs
-  
-  vari.o13C <- std1$vari13CCO2_ref + std1$conc13CCO2_obs^2*vari.g13C + 
-    gain13C^2*std1$vari13CCO2_obs
+  # # calculate uncertainty on gain values.
+  # vari.g12C <- (std1$vari12CCO2_ref + std2$vari12CCO2_ref)/ # already variance, so already sigma^2
+  #   (std1$conc12CCO2_obs - std2$conc12CCO2_obs)^2 +
+  #   (std1$vari12CCO2_obs + std2$vari12CCO2_obs)* # already variance, so already sigma^2
+  #   ((std1$conc12CCO2_ref - std2$conc12CCO2_ref)/(std1$conc12CCO2_obs - std2$conc12CCO2_obs)^2)^2
+  # 
+  # vari.g13C <- (std1$vari13CCO2_ref + std2$vari13CCO2_ref)/ # already variance, so already sigma^2
+  #   (std1$conc13CCO2_obs - std2$conc13CCO2_obs)^2 +
+  #   (std1$vari13CCO2_obs + std2$vari13CCO2_obs)* # already variance, so already sigma^2
+  #   ((std1$conc13CCO2_ref - std2$conc13CCO2_ref)/(std1$conc13CCO2_obs - std2$conc13CCO2_obs)^2)^2
+  # 
+  # vari.o12C <- std1$vari12CCO2_ref + std1$conc12CCO2_obs^2*vari.g12C + 
+  #   gain12C^2*std1$vari12CCO2_obs
+  # 
+  # vari.o13C <- std1$vari13CCO2_ref + std1$conc13CCO2_obs^2*vari.g13C + 
+  #   gain13C^2*std1$vari13CCO2_obs
   
   # return a list of gain and uncertainty values.
-  return(data.frame(gain12C,vari.g12C,gain13C,vari.g13C,offset12C,vari.o12C,offset13C,vari.o13C))
+  return(data.frame(gain12C,gain13C,offset12C,offset13C))
   
 }
