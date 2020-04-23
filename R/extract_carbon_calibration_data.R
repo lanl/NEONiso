840c94e2efe13c03569f6bba856cf98dcd5a1c46
Rich Fiorella <rich.fiorella@utah.edu>
2020-04-23 15:41:44 -0600

    lintr passes on various "utility" style functions.

--- R/extract_carbon_calibration_data.R
+++ R/extract_carbon_calibration_data.R
@@ -1,73 +1,75 @@
 #' extract_carbon_calibration_data.R
 #'
-#' @param standard String indicating whether to grab data from the high, medium, or low standard.
-#' @param data.list List containing data, from the /*/dp01/data/ group in NEON HDF5 file.
-#' @param ucrt.list List containing uncertainty data, from the /*/dp01/ucrt/ group in NEON HDF5 file.
-#' @param ucrt.source Where in the HDF5 file should the variance be extracted from? (Valid values are "data" and "ucrt")
+#' @param standard String indicating whether to grab data from the high,
+#'                  medium, or low standard.
+#' @param data.list List containing data, from the /*/dp01/data/
+#'                  group in NEON HDF5 file.
+#' @param ucrt.list List containing uncertainty data, from the /*/dp01/ucrt/
+#'                  group in NEON HDF5 file.
+#' @param ucrt.source Where from HDF5 file should  variance be extracted from?
+#'                    (Valid values are "data" and "ucrt")
 #'
-#' @return Returns data frame of required variables from /data/ and /ucrt/ groups.
+#' @return Returns data frame of required variables.
 #' @export
 #'
 #' @examples
-extract_carbon_calibration_data <- function(data.list,ucrt.list,standard,ucrt.source="data") {
-  
-  require(dplyr)
-  
+extract_carbon_calibration_data <- function(data_list, ucrt_list,
+                                            standard, ucrt_source = "data") {
+
   if (standard == "high") {
-    data <- data.list$co2High_09m
-    ucrt <- ucrt.list$co2High_09m
+    data <- data_list$co2High_09m
+    ucrt <- ucrt_list$co2High_09m
   } else if (standard == "med") {
-    data <- data.list$co2Med_09m
-    ucrt <- ucrt.list$co2Med_09m
+    data <- data_list$co2Med_09m
+    ucrt <- ucrt_list$co2Med_09m
   } else if (standard == "low") {
-    data <- data.list$co2Low_09m
-    ucrt <- ucrt.list$co2Low_09m
+    data <- data_list$co2Low_09m
+    ucrt <- ucrt_list$co2Low_09m
   } else {
     stop()
   }
-  
-  if (ucrt.source == "ucrt") {
-    
-    std.df <- data.frame(d13C_obs_mean=data$dlta13CCo2$mean,
-                         d13C_obs_n=data$dlta13CCo2$numSamp,
-                         d13C_obs_btime=data$dlta13CCo2$timeBgn,
-                         d13C_obs_etime=data$dlta13CCo2$timeEnd,
-                         d13C_ref_mean=data$dlta13CCo2Refe$mean,
-                         d13C_ref_btime=data$dlta13CCo2Refe$timeBgn,
-                         d13C_ref_etime=data$dlta13CCo2Refe$timeEnd,
-                         d13C_obs_var=ucrt$dlta13CCo2$vari,
-                         d13C_ref_var=data$dlta13CCo2Refe$vari,
-                         CO2_obs_mean=data$rtioMoleDryCo2$mean,
-                         CO2_obs_n=data$rtioMoleDryCo2$numSamp,
+
+  if (ucrt_source == "ucrt") {
+
+    std_df <- data.frame(d13C_obs_mean = data$dlta13CCo2$mean,
+                         d13C_obs_n = data$dlta13CCo2$numSamp,
+                         d13C_obs_btime = data$dlta13CCo2$timeBgn,
+                         d13C_obs_etime = data$dlta13CCo2$timeEnd,
+                         d13C_ref_mean = data$dlta13CCo2Refe$mean,
+                         d13C_ref_btime = data$dlta13CCo2Refe$timeBgn,
+                         d13C_ref_etime = data$dlta13CCo2Refe$timeEnd,
+                         d13C_obs_var = ucrt$dlta13CCo2$vari,
+                         d13C_ref_var = data$dlta13CCo2Refe$vari,
+                         CO2_obs_mean = data$rtioMoleDryCo2$mean,
+                         CO2_obs_n = data$rtioMoleDryCo2$numSamp,
+                         CO2_ref_mean = data$rtioMoleDryCo2Refe$mean,
+                         CO2_ref_var = data$rtioMoleDryCo2Refe$vari,
+                         CO2_obs_var = ucrt$rtioMoleDryCo2$vari)
+
+  } else if (ucrt_source == "data") {
+
+    std_df <- data.frame(d13C_obs_mean = data$dlta13CCo2$mean,
+                         d13C_obs_n = data$dlta13CCo2$numSamp,
+                         d13C_obs_btime = data$dlta13CCo2$timeBgn,
+                         d13C_obs_etime = data$dlta13CCo2$timeEnd,
+                         d13C_ref_mean = data$dlta13CCo2Refe$mean,
+                         d13C_ref_btime = data$dlta13CCo2Refe$timeBgn,
+                         d13C_ref_etime = data$dlta13CCo2Refe$timeEnd,
+                         d13C_obs_var = data$dlta13CCo2$vari,
+                         d13C_ref_var = data$dlta13CCo2Refe$vari,
+                         CO2_obs_mean = data$rtioMoleDryCo2$mean,
+                         CO2_obs_n = data$rtioMoleDryCo2$numSamp,
                          CO2_ref_mean=data$rtioMoleDryCo2Refe$mean,
-                         CO2_ref_var=data$rtioMoleDryCo2Refe$vari,
-                         CO2_obs_var=ucrt$rtioMoleDryCo2$vari)
-    
-  } else if (ucrt.source == "data") {
-    
-    std.df <- data.frame(d13C_obs_mean=data$dlta13CCo2$mean,
-                         d13C_obs_n=data$dlta13CCo2$numSamp,
-                         d13C_obs_btime=data$dlta13CCo2$timeBgn,
-                         d13C_obs_etime=data$dlta13CCo2$timeEnd,
-                         d13C_ref_mean=data$dlta13CCo2Refe$mean,
-                         d13C_ref_btime=data$dlta13CCo2Refe$timeBgn,
-                         d13C_ref_etime=data$dlta13CCo2Refe$timeEnd,
-                         d13C_obs_var=data$dlta13CCo2$vari,
-                         d13C_ref_var=data$dlta13CCo2Refe$vari,
-                         CO2_obs_mean=data$rtioMoleDryCo2$mean,
-                         CO2_obs_n=data$rtioMoleDryCo2$numSamp,
-                         CO2_ref_mean=data$rtioMoleDryCo2Refe$mean,
-                         CO2_ref_var=data$rtioMoleDryCo2Refe$vari,
-                         CO2_obs_var=data$rtioMoleDryCo2$vari)
-    
+                         CO2_ref_var = data$rtioMoleDryCo2Refe$vari,
+                         CO2_obs_var = data$rtioMoleDryCo2$vari)
+
   }
 
-  
   # add standard name
-  std.df <- std.df %>%
-    mutate(std_name=standard)
+  std_df <- std_df %>%
+    mutate(std_name = standard)
 
   # return standard data frame.
   return(std.df)
-  
+
 }
--- R/swap_standard_isotoperatios.R
+++ R/swap_standard_isotoperatios.R
@@ -1,27 +1,29 @@
 #' Title
 #'
-#' @param std.frame 
-#' @param dxs.thres 
+#' @author Rich Fiorella \email{rich.fiorella@@utah.edu}
+#'
+#' @param std.frame Standard data frame to perform swap on.
+#' @param dxs.thres d-excess threshold to indicate when to swap.
 #'
 #' @noRd
 
-swap_standard_isotoperatios <- function(std.frame,dxs.thres = 500) {
+swap_standard_isotoperatios <- function(std_frame, dxs_thres = 500) {
   # calculate d excess
-  dxs <- std.frame$d2H_ref_mean - 8*std.frame$d18O_ref_mean
+  dxs <- std_frame$d2H_ref_mean - 8 * std_frame$d18O_ref_mean
   
   # if d-excess is extremely positive, indicates a really low d18O value for
-  # a given d2H value, and potentially switched isotope ratios in the standard.
-  inds = which(dxs > dxs.thres)
-  
-  tmpa <- std.frame$d2H_ref_mean
-  tmpb <- std.frame$d18O_ref_mean
-  
+  # a given d2H value, and potentially switched isotope ratios in the standard
+  inds <- which(dxs > dxs_thres)
+
+  tmpa <- std_frame$d2H_ref_mean
+  tmpb <- std_frame$d18O_ref_mean
+
   if (sum(inds) > 0) {
     # swap isotope ratios where inds = TRUE
-    std.frame$d2H_ref_mean[inds] <- tmpb[inds]
-    std.frame$d18O_ref_mean[inds] <- tmpa[inds]
+    std_frame$d2H_ref_mean[inds] <- tmpb[inds]
+    std_frame$d18O_ref_mean[inds] <- tmpa[inds]
   }
-  
+
   # return fixed dataframe
-  return(std.frame)
+  return(std_frame)
 }
