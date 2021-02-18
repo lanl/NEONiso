b9e49b7d0c310aceab2e868ab2e83d3597b9f968
Rich Fiorella <rich.fiorella@utah.edu>
2021-02-17 21:37:53 -0700

    Add second standard_corrections test

--- tests/testthat/test-standard_corrections.R
+++ tests/testthat/test-standard_corrections.R
@@ -5,3 +5,10 @@ ref_data <- data.frame(d2H_ref_mean  = c(-80,-100,-120),
 test_that("reference water with reasonable d-excess is unchanged", {
   expect_equal(swap_standard_isotoperatios(ref_data),ref_data)
 })
+
+ref_data2 <- data.frame(d18O_ref_mean  = c(-80,-100,-120),
+                       d2H_ref_mean = c(-10,-11,-12))
+
+test_that("unreasonable d-excess values get swapped", {
+  expect_equal(swap_standard_isotoperatios(ref_data2),ref_data)
+})
