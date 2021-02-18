6a3a5e8b1da32035c2904ea90be0b17666df905e
Rich Fiorella <rich.fiorella@utah.edu>
2021-02-17 21:21:47 -0700

    Add first test to make sure testing workflow is working.

---
+++ tests/testthat/test-standard_corrections.R
@@ -0,0 +1,7 @@
+
+ref_data <- data.frame(d2H_ref_mean  = c(-80,-100,-120),
+                       d18O_ref_mean = c(-10,-11,-12))
+
+test_that("reference water with reasonable d-excess is unchanged", {
+  expect_equal(swap_standard_isotoperatios(ref_data),ref_data)
+})
