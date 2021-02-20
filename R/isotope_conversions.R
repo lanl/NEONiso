391982474d053b30b7af5cf0415eb4ea64f85939
Rich Fiorella <rich.fiorella@utah.edu>
2021-02-19 20:41:36 -0700

    Add file for various isotope conversions.

---
+++ R/isotope_conversions.R
@@ -0,0 +1,14 @@
+# isotope functions
+# break out functions that are various transformations
+# of isotope ratios and delta values.
+#------------------------------------------------------
+R_to_delta <- function(data_vector,element) {
+  
+  
+  
+}
+
+
+delta_to_R <- function(data_vector,element) {
+  
+}
