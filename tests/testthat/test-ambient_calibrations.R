8e56df2b4dbc7101361a67a66a198e729075c689
Rich Fiorella <rich.fiorella@utah.edu>
2021-02-26 15:23:14 -0700

    Start to build test infrastructure for ambient calibrations.
    Will ultimately need to move the write to hdf5 functions out to their own file.

---
+++ tests/testthat/test-ambient_calibrations.R
@@ -0,0 +1,8 @@
+# test-ambient_calibrations
+
+load("amb_co2_data.Rdata")
+load("carbon_GO_cals.Rdata")
+
+test_that("calibrate_ambient_carbon_Bowling2003 errors out when bad output file name is returned.", {
+  expect_error(calibrate_ambient_carbon_Bowling2003(amb_co2_data, carbon_GO_cals, "dlta13CCo2", "ONAQ", "test.h5"))
+})
