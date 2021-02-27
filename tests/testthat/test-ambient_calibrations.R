7d6aad3624e3fe492240a1567c9a0eb454992114
Rich Fiorella <rich.fiorella@utah.edu>
2021-02-26 22:28:24 -0700

    Pushing final friday tweaks

--- tests/testthat/test-ambient_calibrations.R
+++ tests/testthat/test-ambient_calibrations.R
@@ -4,5 +4,10 @@ load("amb_co2_data.Rdata")
 load("carbon_GO_cals.Rdata")
 
 test_that("calibrate_ambient_carbon_Bowling2003 errors out when bad output file name is returned.", {
-  expect_error(calibrate_ambient_carbon_Bowling2003(amb_co2_data, carbon_GO_cals, "dlta13CCo2", "ONAQ", "test.h5"))
+  expect_error(calibrate_ambient_carbon_Bowling2003(amb_co2_data, carbon_GO_cals, 
+                                                    "dlta13CCo2", "ONAQ", "test.h5",
+                                                    force_to_end = FALSE,
+                                                    force_to_beginning = FALSE,
+                                                    filter_data = FALSE,
+                                                    r2_thres = 0.95))
 })
