1830c31200e58f87d7b6652e9bd50cd0be5f65ab
Rich Fiorella <rich.fiorella@utah.edu>
2021-03-02 23:12:22 -0700

    Update ambient calibration tests.

--- tests/testthat/test-ambient_calibrations.R
+++ tests/testthat/test-ambient_calibrations.R
@@ -2,11 +2,16 @@
 
 load("co2RawAmbData.Rdata")
 load("co2B03CalAmbData.Rdata")
+load("co2B03CalTable.Rdata")
+
+co2B03CalTablePosix <- co2B03CalTable
+co2B03CalTablePosix$start <- convert_NEONhdf5_to_POSIXct_time(co2B03CalTablePosix$valid_period_start)
+co2B03CalTablePosix$end   <- convert_NEONhdf5_to_POSIXct_time(co2B03CalTablePosix$valid_period_end)
 
 test_that("calibrate_ambient_carbon_Bowling2003 errors out when bad output file name is returned.", {
-  expect_error(calibrate_ambient_carbon_Bowling2003(co2RawAmbData, co2B03CalAmbData, "dlta13CCo2", "ONAQ", "test.h5"))
+  expect_error(calibrate_ambient_carbon_Bowling2003(co2RawAmbData, co2B03CalTablePosix, "dlta13CCo2", "ONAQ", "test.h5"))
 })
 
-#test_that("calibrate_ambient_carbon_Bowling2003 finishes when bad output file name is returned.", {
-calibrate_ambient_carbon_Bowling2003(co2RawAmbData, co2B03CalAmbData,"dlta13CCo2", "ONAQ", 'test.h5', write_to_file = FALSE)
-#})
+test_that("calibrate_ambient_carbon_Bowling2003 finishes when bad output file name is returned.", {
+  expect_silent(calibrate_ambient_carbon_Bowling2003(co2RawAmbData, co2B03CalTablePosix,"dlta13CCo2", "ONAQ", 'test.h5', write_to_file = FALSE))
+})
