7a3546db160c33f049f356eda5132e9e2338dfb6
Rich Fiorella <rich.fiorella@utah.edu>
2021-06-11 11:05:43 -0600

    Add more tests of Co2 data ingestion functions

--- tests/testthat/test-data_ingestion.R
+++ tests/testthat/test-data_ingestion.R
@@ -7,6 +7,12 @@ fin <- system.file('extdata',
 #------------------------------------------
 # various tests about output structure of ingest_data.
 
+# TEMPORARY TESTS:
+# expect failure if analyte = 'H2o'
+test_that("ingest_data errors for H2o (TEMPORARY)", {
+  expect_error(ingest_data(fin, analyte = 'H2o'))
+})
+
 # 1) for CO2:
 co2test <- ingest_data(fin, analyte = 'Co2')
 
@@ -20,4 +26,31 @@ test_that("ingest_data returns list with correct sublist names", {
   expect_equal(names(co2test), c("ambient", "reference", "refe_stacked"))
 })
 
+# test restructure_carbon_variables function
+rest_raw <- neonUtilities::stackEddy(fin, avg = 9, level = 'dp01')[[1]] %>%
+  dplyr::select(.data$verticalPosition, .data$timeBgn, 
+                .data$timeEnd, tidyselect::contains('isoCo2'))
+
+rest_raw <- rest_raw[rowSums(is.na(rest_raw)) < 145, ] # Needed to remove merge error where times are slightly different between h2o and co2.
+
+test_that("restructure_carbon_variables errors when invalid mode provided", {
+  expect_error(restructure_carbon_variables(rest_raw, 'dlta13CCo2', mode = 'cheese', group = 'data'))
+  expect_silent(restructure_carbon_variables(rest_raw, 'dlta13CCo2', mode = 'reference', group = 'data'))
+  expect_silent(restructure_carbon_variables(rest_raw, 'dlta13CCo2', mode = 'ambient', group = 'data'))
+})
+
+test_that("output structure from restructure_carbon_variables is correct", {
+  expect_true(is.data.frame(restructure_carbon_variables(rest_raw, 'dlta13CCo2', mode = 'reference', group = 'data')))
+  expect_equal(ncol(restructure_carbon_variables(rest_raw, 'dlta13CCo2', mode = 'reference', group = 'data')), 7)
+  expect_equal(ncol(restructure_carbon_variables(rest_raw, 'dlta13CCo2', mode = 'ambient', group = 'data')), 7)
+})
+
+test_that("output datetimes from restructure_carbon_variables are characters, not POSIX", {
+  tmp <- restructure_carbon_variables(rest_raw, 'dlta13CCo2', mode = 'ambient', group = 'data')
+  expect_type(tmp$timeBgn, 'character')
+  expect_type(tmp$timeEnd, 'character')
+  tmp <- restructure_carbon_variables(rest_raw, 'dlta13CCo2', mode = 'reference', group = 'data')
+  expect_type(tmp$timeBgn, 'character')
+  expect_type(tmp$timeEnd, 'character')
+})
 
