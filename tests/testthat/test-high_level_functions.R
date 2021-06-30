6706f3174aa6d0bdd526846784e328474efc8601
Rich Fiorella <rich.fiorella@utah.edu>
2021-06-30 17:00:54 -0600

    Update a test.

--- tests/testthat/test-high_level_functions.R
+++ tests/testthat/test-high_level_functions.R
@@ -5,13 +5,16 @@
 # all other tests pass, but good practice to include these in case a 
 # stray character enters these functions somehow.
 
+# 210630: these tests actually don't do much right now, other than increase coverage!
+# could spend some time actually having these test important functionality.
+
 fin <- system.file('extdata',
                    'NEON.D15.ONAQ.DP4.00200.001.nsae.2019-05.basic.packed.h5',
                    package = 'NEONiso', mustWork = TRUE)
 
 fout <- '/dev/null'
 
-test_that("calibrate_carbon returns silently if write_to_file = FALSE" ,{
+test_that("calibrate_carbon returns 'Extracting data' if write_to_file = FALSE" ,{
   
   # these tests could probably be made more useful!!
   expect_output(calibrate_carbon(fin, fout, "ONAQ",
@@ -36,4 +39,31 @@ test_that("calibrate_carbon returns silently if write_to_file = FALSE" ,{
                    correct_refData = TRUE,
                    write_to_file = FALSE), "Extracting data")
   
+})
+
+
+test_that("calibrate_carbon_bymonth returns 'Extracting data' if write_to_file = FALSE", {
+  
+  # these tests could probably be made more useful!!
+  expect_output(calibrate_carbon_bymonth(fin, fout, "ONAQ",
+                                 method = "Bowling_2003",
+                                 calibration_half_width = 0.5,
+                                 force_cal_to_beginning = TRUE,
+                                 force_cal_to_end = TRUE,
+                                 gap_fill_parameters = FALSE,
+                                 filter_ambient = TRUE,
+                                 r2_thres = 0.95,
+                                 correct_refData = TRUE,
+                                 write_to_file = FALSE), "Extracting data")
+  
+  expect_output(calibrate_carbon_bymonth(fin, fout, "ONAQ",
+                                 method = "linreg",
+                                 calibration_half_width = 0.5,
+                                 force_cal_to_beginning = TRUE,
+                                 force_cal_to_end = TRUE,
+                                 gap_fill_parameters = FALSE,
+                                 filter_ambient = TRUE,
+                                 r2_thres = 0.95,
+                                 correct_refData = TRUE,
+                                 write_to_file = FALSE), "Extracting data")
 })
