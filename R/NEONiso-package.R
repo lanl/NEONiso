3ce6b1acc09822821be39d622adfc449071545ab
Rich Fiorella <rfiorella@lanl.gov>
2023-08-23 11:03:26 -0600

    update package documentation based on CRAN request

---
+++ R/NEONiso-package.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+# suppress R CMD CHECK notes on variables that are internal to this package.
+globalVariables(c("..density..", "varname"))
